7e9ab3ff05435056efb3b777cae74a026e47c42f
Oscar A. Reyes <oscarreyes2194@gmail.com>
2022-05-03 21:19:52 -0500

    styled navigation with responsiveness

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -2,37 +2,59 @@ import { Link, Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { selectCurrentUser } from "../../store/user/user.selector";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
-import "./navigation.styles.scss";
-import { Fragment } from "react";
+import {
+  NavItems,
+  NavLinkItem,
+  SignOutButton,
+  Push,
+  NavLink,
+  HamburgerIcon,
+  HamburgerIconLine1,
+  HamburgerIconLine2,
+  HamburgerIconLine3,
+  HamburgerMenuDiv,
+} from "./navigation.styles.js";
+import { Fragment, useState } from "react";
 
 const Navigation = () => {
   const currentUser = useSelector(selectCurrentUser);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+    console.log(isOpen);
+  };
 
   return (
     <Fragment>
-      <nav className="nav_container">
-        <Link to="/">
-          <div className="logo">Logo</div>
-        </Link>
-        <ul className="nav-items">
-          <li className="nav-link-item">
-            <Link to="/icos">ICOs</Link>
-          </li>
-          <li className="nav-link-item">
-            <Link to="/watchlist">WatchList</Link>
-          </li>
+      <nav className="nav-container">
+        <HamburgerMenuDiv  onClick={toggleMenu}>
+          <HamburgerIconLine1 open={isOpen}></HamburgerIconLine1>
+          <HamburgerIconLine2 open={isOpen}></HamburgerIconLine2>
+          <HamburgerIconLine3 open={isOpen}></HamburgerIconLine3>
+        </HamburgerMenuDiv>
+        <NavItems>
+          <Push>
+            <Link to="/">
+              <div className="logo">Logo</div>
+            </Link>
+          </Push>
+          <NavLinkItem open={isOpen}>
+            <NavLink to="/icos">ICOs</NavLink>
+          </NavLinkItem>
+          <NavLinkItem open={isOpen}>
+            <NavLink to="/watchlist">WatchList</NavLink>
+          </NavLinkItem>
           {currentUser ? (
-            <li className="nav-link-item">
-              <span className="sign-out-button" onClick={signOutUser}>
-                Sign Out
-              </span>
-            </li>
+            <NavLinkItem open={isOpen}>
+              <SignOutButton onClick={signOutUser}>Sign Out</SignOutButton>
+            </NavLinkItem>
           ) : (
-            <li className="nav-link-item">
-              <Link to="/auth">Sign In</Link>
-            </li>
+            <NavLinkItem open={isOpen}>
+              <NavLink to="/auth">Sign In</NavLink>
+            </NavLinkItem>
           )}
-        </ul>
+        </NavItems>
       </nav>
       <Outlet />
     </Fragment>
---
+++ src/routes/navigation/navigation.styles.js
@@ -0,0 +1,117 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const NavItems = styled.ul`
+  display: flex;
+  list-style: none;
+  justify-content: flex-end;
+  text-align: right;
+  flex-wrap: wrap;
+  padding: 0px 10px 3px;
+  z-index: 5;
+  box-shadow: 0px 5px 4px #bcbcbc;
+  @media only screen and (max-width: 439px) {
+    justify-content: space-around;
+    flex-direction: column;
+    text-align: left;
+  }
+`;
+
+export const NavLinkItem = styled.li`
+  padding: 10px 20px;
+  @media only screen and (max-width: 439px) {
+    display: ${({ open }) => (open ? "block" : "none")};
+  }
+`;
+
+export const SignOutButton = styled.span`
+  cursor: pointer;
+  font-size: 1.1em;
+  &:hover {
+    color: #07a3e3;
+  }
+`;
+
+export const Push = styled(NavLinkItem)`
+  margin-right: auto;
+  @media only screen and (max-width: 439px) {
+    padding-bottom: 16px;
+    display: block;
+  }
+`;
+
+export const NavLink = styled(Link)`
+  font-size: 1.1em;
+  &:hover {
+    color: #07a3e3;
+  }
+`;
+
+export const HamburgerIcon = styled.div`
+  width: 35px;
+  height: 5px;
+  background-color: black;
+  margin: 6px 0;
+  z-index: 7;
+  position: absolute;
+  right: 12px;
+  display: none;
+  @media only screen and (max-width: 439px) {
+    display: block;
+  }
+`;
+
+export const HamburgerIconLine1 = styled(HamburgerIcon)`
+  top: 18px;
+  transform: ${({ open }) =>
+    open ? "rotate(45deg) translate(8px, 8px)" : "rotate(0)"};
+`;
+
+export const HamburgerIconLine2 = styled(HamburgerIcon)`
+  top: 29px;
+  display: ${({ open }) => (open ? "none" : "block")};
+`;
+export const HamburgerIconLine3 = styled(HamburgerIcon)`
+  top: 40px;
+  transform: ${({ open }) =>
+    open ? "rotate(-45deg) translate(8px, -7.5px)" : "rotate(0)"};
+`;
+
+export const HamburgerMenuDiv = styled.div`
+  position: absolute;
+  top: 0px;
+  right: 0;
+  height: 62px;
+  width: 60px;
+  display: none;
+  &:hover {
+    cursor: pointer;
+  }
+
+  @media only screen and (max-width: 439px) {
+    display: block;
+  }
+`;
+// .nav-container {
+//   .nav-items {
+//     display: flex;
+//     list-style: none;
+//     justify-content: flex-end;
+//     text-align: right;
+//     flex-wrap: wrap;
+//     padding: 0 10px;
+
+//     .nav-link-item {
+//       padding: 10px 20px;
+//     }
+
+//     .push {
+//       margin-right: auto;
+//     }
+
+//     .sign-out-button {
+//       cursor: pointer;
+//     }
+//   }
+
+// }
