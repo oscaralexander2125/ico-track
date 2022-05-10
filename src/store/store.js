15438d286c0ab6c6f3e9b7e1f66c50e17468a8e5
Oscar A. Reyes <oscarreyes2194@gmail.com>
2022-05-10 11:18:41 -0500

    changed logger only when not in production

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,19 @@
 import { compose, createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import { rootReducer } from "./root-reducer";
+import thunk from "redux-thunk";
 
-const middleWares = [logger];
+const middleWares = [
+  process.env.NODE_ENV !== "production" && logger,
+  thunk,
+].filter(Boolean);
 
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+const composeEnhancer =
+  (process.env.NODE_ENV !== "production" &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
 export const store = createStore(rootReducer, undefined, composedEnhancers);
